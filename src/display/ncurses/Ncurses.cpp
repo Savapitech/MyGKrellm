85a5897c4eadcd03ed4a06fa3d3bb7e99a1e7ef1
Titou <titouan.hochet@epitech.eu>
2026-01-31 11:24:24 +0100

    draw text and box for ncurses with base display but needs to be reworked

---
+++ src/display/ncurses/Ncurses.cpp
@@ -0,0 +1,56 @@
+#include "Ncurses.hpp"
+#include <ncurses.h>
+
+Ncurses::Ncurses() {
+  _name = "Ncurses";
+}
+
+Ncurses::~Ncurses() {
+  endwin();
+
+}
+
+void Ncurses::init() {
+    _window = initscr();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(3, COLOR_GREEN, COLOR_BLACK);
+    init_pair(4, COLOR_YELLOW, COLOR_BLACK);
+    cbreak();
+    noecho();
+}
+
+
+void Ncurses::draw_box(int x, int y, int width, int heigth) {
+  attron(COLOR_PAIR(1));
+  heigth /= 2;
+  y /= 2;
+
+  mvhline(y, x + 1, ACS_HLINE, width - 1);
+  mvvline(y + 1, x, ACS_VLINE, heigth - 1);
+  mvvline(y + 1, x + width, ACS_VLINE, heigth - 1);
+  mvhline(y + heigth, x + 1, ACS_HLINE, width - 1);
+
+  mvaddch(y + heigth, x, ACS_LLCORNER);
+  mvaddch(y + heigth, x + width, ACS_LRCORNER);
+  mvaddch(y, x, ACS_ULCORNER);
+  mvaddch(y, x + width, ACS_URCORNER);
+}
+
+void Ncurses::draw_text(int x, int y, std::string text) {
+  y /= 2;
+  attron(COLOR_PAIR(2));
+  mvprintw(y, x, " %s ", text.c_str());
+}
+
+
+void Ncurses::display_window() {
+  refresh();
+  getch();
+}
+
+void Ncurses::refresh() {
+  getch();
+}
+
--- src/display/ncurses/Ncurses.hpp
+++ src/display/ncurses/Ncurses.hpp
@@ -1,1 +1,15 @@
 #include "../ADisplay.hpp"
+#include "ncurses.h"
+
+class Ncurses : public ADisplay {
+  public:
+    Ncurses();
+    virtual ~Ncurses();
+    virtual void init() override;
+    virtual void display_window() override; 
+    virtual void refresh() override;
+    virtual void draw_box(int x, int y, int width, int height) override;
+    virtual void draw_text(int x, int y, std::string text) override; 
+  private:
+    WINDOW *_window;
+};
