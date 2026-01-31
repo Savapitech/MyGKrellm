e1bbc692466d8019524925c7e9713a7f1195a522
Titou <titouan.hochet@epitech.eu>
2026-01-31 15:42:52 +0100

    [ADD] ram to the main

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,33 @@
 #include "modules/IModule.hpp"
 #include "modules/ModuleCpuInfo.hpp"
 #include "modules/ModuleSystemInfo.hpp"
-#include "modules/ModuleCpuInfo.hpp"
+#include "modules/ModuleRamInfo.hpp"
 
-int mainLoop(IDisplay *disp, IModule *sy, IModule *cpu)
+int mainLoop(IDisplay *disp, IModule *sy, IModule *cpu, IModule *ram)
 {
     while (disp->getState()) {
       disp->refreshWindow();
       sy->update();
       cpu->update();
+      ram->update();
       sy->draw(*disp);
       cpu->draw(*disp);
+      ram->draw(*disp);
       disp->displayWindow();
-      sleep(1);
     }
     return 1;
 }
 
 int main(void) {
   Ncurses nc;
-  SFML sf;
-  IDisplay *disp = &sf;
+  //SFML sf;
+  IDisplay *disp = &nc;
   IModule *sy = new ModuleSystemInfo();
   IModule *cpu = new ModuleCpuInfo();
+  IModule *ram = new ModuleRamInfo();
 
   disp->init();
-  while (mainLoop(disp, sy, cpu)) {
+  while (mainLoop(disp, sy, cpu, ram)) {
     disp->cleanup();
     disp = &nc;
     disp->init();
