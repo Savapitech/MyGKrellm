761ceba00596067040c1175f0fd774fac53594bd
Gabriel Viel <gabriel.viel@epitech.eu>
2026-01-31 15:17:19 +0100

    Added extra parameters to the drawBar method

--- src/display/IDisplay.hpp
+++ src/display/IDisplay.hpp
@@ -10,7 +10,7 @@ class IDisplay {
     virtual void refreshWindow() = 0;
     virtual void drawBox(int x, int y, int width, int height) = 0;
     virtual void drawText(int x, int y, std::string text) = 0;
-    virtual void drawBar(int x, int y, uint8_t percentage, std::string text) = 0;
+    virtual void drawBar(int x, int y, int height, int width, uint8_t percentage, std::string text) = 0;
     virtual void cleanup() = 0;
     virtual bool getState() const = 0;
 };
